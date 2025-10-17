b93a4c89304d5249b497bd34355c36717834408f
Hanserprpr <2041283903@qq.com>
2025-10-17 12:21:33 +0800

    更新redis

--- src/main/java/org/stnhh/sdu_flea_market/config/RedisConfig.java
+++ src/main/java/org/stnhh/sdu_flea_market/config/RedisConfig.java
@@ -1,66 +1,19 @@
 package org.stnhh.sdu_flea_market.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.stnhh.sdu_flea_market.cache.IGlobalCache;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.stnhh.sdu_flea_market.cache.impl.AppRedisCacheManager;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
-
-import java.time.Duration;
 
 @EnableCaching
 @Configuration
 public class RedisConfig {
-
-    @Value("${spring.data.redis.host}")
-    private String host;
-    @Value("${spring.data.redis.database}")
-    private Integer database;
-    @Value("${spring.data.redis.port}")
-    private Integer port;
-    @Value("${spring.data.redis.password}")
-    private String pwd;
-
-    @Primary
-    @Bean(name = "jedisPoolConfig")
-    @ConfigurationProperties(prefix = "spring.redis.pool")
-    public JedisPoolConfig jedisPoolConfig() {
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxTotal(200); // 最大连接数
-        jedisPoolConfig.setMaxIdle(10);  // 最大空闲连接
-        jedisPoolConfig.setMinIdle(2);   // 最小空闲连接
-        jedisPoolConfig.setBlockWhenExhausted(true); // 连接池耗尽时，是否等待可用连接
-        jedisPoolConfig.setMaxWait(Duration.ofMillis(10000)); // 设置最大等待时间
-        return jedisPoolConfig;
-    }
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory(JedisPoolConfig jedisPoolConfig) {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setDatabase(database);
-        redisStandaloneConfiguration.setPassword(pwd);
-        redisStandaloneConfiguration.setPort(port);
-        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder jpcb = (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration.builder();
-        jpcb.poolConfig(jedisPoolConfig);
-        JedisClientConfiguration jedisClientConfiguration = jpcb.build();
-        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
-    }
 
     @Primary
     @Bean(name = "redisTemplate")
@@ -68,21 +21,15 @@ public class RedisConfig {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
 
-        // Key 和 HashKey 采用 String 序列化器
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        template.setKeySerializer(stringRedisSerializer);
-        template.setHashKeySerializer(stringRedisSerializer);
+        // key/hashKey 用 String
+        StringRedisSerializer stringSer = new StringRedisSerializer();
+        template.setKeySerializer(stringSer);
+        template.setHashKeySerializer(stringSer);
 
-        // Value 和 HashValue 采用 Jackson2JsonRedisSerializer
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL,
-                JsonTypeInfo.As.PROPERTY);
-
-        Jackson2JsonRedisSerializer<Object> redisSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
-
-        template.setValueSerializer(redisSerializer);
-        template.setHashValueSerializer(redisSerializer);
+        // value/hashValue 用通用 JSON 序列化（更安全，免去 default typing 安全隐患）
+        GenericJackson2JsonRedisSerializer jsonSer = new GenericJackson2JsonRedisSerializer();
+        template.setValueSerializer(jsonSer);
+        template.setHashValueSerializer(jsonSer);
 
         template.afterPropertiesSet();
         return template;
@@ -94,7 +41,8 @@ public class RedisConfig {
     }
 
     @Bean
-    IGlobalCache cache(RedisTemplate<String, Object> redisTemplate, HashOperations<String, String, String> hashOperations) {
+    AppRedisCacheManager cache(RedisTemplate<String, Object> redisTemplate,
+                               HashOperations<String, String, String> hashOperations) {
         return new AppRedisCacheManager(redisTemplate);
     }
 }
